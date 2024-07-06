e7eb50a2c151456012fe9df7a313674e261cc421
Rolando HR <rolando167@hotmail.com>
2024-07-06 18:49:56 +0200

    RH  06 - ca many to many

--- src/main/java/com/api/h2/Entities/Venta.java
+++ src/main/java/com/api/h2/Entities/Venta.java
@@ -5,15 +5,12 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
