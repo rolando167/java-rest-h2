979be3590b905af39b3c2c2cf973983f33cdc937
Rolando HR <rolando167@hotmail.com>
2024-08-20 18:47:07 +0200

    RH  20 - pruebas

--- src/main/java/com/api/h2/Services/ClienteService.java
+++ src/main/java/com/api/h2/Services/ClienteService.java
@@ -2,6 +2,7 @@ package com.api.h2.Services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,37 +16,41 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public List<Cliente> getAll(){
+    public List<Cliente> getAll() {
         return clienteRepository.findAll();
     }
 
-    public Optional<Cliente> find(Long id){
+    public Optional<Cliente> find(Long id) {
         return clienteRepository.findById(id);
     }
 
-    public Cliente save(Cliente cliente){
+    public Cliente save(Cliente cliente) {
+        Random rand = new Random();
+        int max = 50;
+        int min = 20;
+        Integer rand_int1 = rand.nextInt(max - min + 1) + min;
+        cliente.setAge(rand_int1.longValue());
         return clienteRepository.save(cliente);
     }
 
-    public Cliente update(Cliente cliente, Long id){
+    public Cliente update(Cliente cliente, Long id) {
         Optional<Cliente> clientefind = clienteRepository.findById(id);
         if (clientefind.isPresent()) {
             Cliente cliente2 = clientefind.get();
             cliente2.setName(cliente.getName());
             cliente2.setLast_name(cliente.getLast_name());
             return clienteRepository.save(cliente2);
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public Cliente delete(long id){
+    public Cliente delete(long id) {
         Optional<Cliente> clientefind = clienteRepository.findById(id);
-        if(clientefind.isPresent()) {
-             clienteRepository.deleteById(id);
-             return clientefind.get();
-        }else{
+        if (clientefind.isPresent()) {
+            clienteRepository.deleteById(id);
+            return clientefind.get();
+        } else {
             return null;
         }
     }
